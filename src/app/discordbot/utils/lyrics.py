ad9fcc49ab459312c52f914d2e07e724a5da89e8
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2019-01-14 22:38:16 -0200

    Lyrics search while playing song

--- src/app/discordbot/utils/lyrics.py
+++ src/app/discordbot/utils/lyrics.py
@@ -15,10 +15,10 @@ class AnimeLyrics:
         self.title:str
         self.lyrics:str
 
-        self.load_results()
+        self.load_results() #Inicializa a busca pela letra
 
     def load_results(self):
-        print(list(search(self.search_term, tld='com.pk', lang='en', stop=10)))
+        print("Searching lyrics for [{}]...".format(self.search_term))
         for url in search(self.search_term, tld='com.pk', lang='en', stop=10):
             if parse(url).hostname == 'www.animelyrics.com':
                 self._parse_animelyrics(url)
@@ -29,7 +29,7 @@ class AnimeLyrics:
             elif parse(url).hostname == 'www.smule.com':
                 self._parse_smule(url)
                 return
-        print(list(search(self.search_term+' lyrics', tld='com.pk', lang='en', stop=10)))
+        print("Searching lyrics for [{}] (2nd try)...".format(self.search_term))
         for url in search(self.search_term+' lyrics', tld='com.pk', lang='en', stop=10):
             if parse(url).hostname == 'www.animelyrics.com':
                 self._parse_animelyrics(url)
@@ -45,18 +45,20 @@ class AnimeLyrics:
         self.page_url = url
         self.website = 'animelyrics'
         self.soup = BeautifulSoup(requests.get(self.page_url).text, 'html5lib')
-        self.lyrics = ', '.join(
-            [div.text.replace('\xa0', ' ').strip() for div in self.soup.find_all('td', {'class': 'romaji'})])
+        self.lyrics = ', '.join([div.text.replace('\xa0', ' ').strip() for div in self.soup.find_all('td', {'class': 'romaji'})])
         self.title = self.soup.find_all('h1')[1].get_text()
+        print("Found [{}] lyrics on animelyrics.com!".format(self.search_term))
 
     def _parse_letras(self, url):
         self.page_url = url
         self.website = 'letras.mus'
         html = urllib.request.urlopen(self.page_url).read().decode()
         self.lyrics = html.split('<article>')[1].split('</article>')[0].replace('<p>', '\n').replace('</p>','\n').replace('<br/>', '\n')
+        print("Found [{}] lyrics on letras.mus.br!".format(self.search_term))
 
     def _parse_smule(self, url):
         self.page_url = url
         self.website = 'smule'
         html = urllib.request.urlopen(self.page_url).read().decode()
         self.lyrics = html.split('<div class="lyrics content ">')[1].split('</div>')[0].replace('<p>', '\n').replace('</p>', '\n').replace('<br>', '\n')
+        print("Found [{}] lyrics on smule.com!".format(self.search_term))
